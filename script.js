42ef9792fe177970a6ae7868c690c80b4cb53fec
Zion-El <mchladelola@gmail.com>
2022-07-06 14:55:41 +0100

    modified commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form')
+const form = document.getElementById('myform')
 const firstname = document.getElementById('firstname')
 const lastname = document.getElementById('lastname')
 const email = document.getElementById('e-mail')
@@ -11,7 +11,20 @@ form.addEventListener('submit', e => {
 });
 
 const setError = (element, message) => {
-    
+    const input = element.parentElement;
+    const errorDisplay = input.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    input.classList.add('error');
+    input.classList.remove('success')
+}
+const setSuccess = element => {
+    const input = element.parentElement;
+    const errorDisplay = input.querySelector('.error');
+ 
+    errorDisplay.innerText = '';
+    input.classList.remove('error');
+    input.classList.add('success')
 }
 
 const validateInputs = () => {
@@ -21,6 +34,26 @@ const validateInputs = () => {
     const passwordValue = password.value.trim();
 
     if (firstnameValue === '') {
-
+        setError(firstname, 'First name is required')
+    } else {
+        setSuccess(firstname)       
     }
-}
+
+    if (lastnameValue === '') {
+        setError(lastname, 'Last name is required')
+    } else {
+        setSuccess(lastname)       
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required')
+    } else {
+        setSuccess(email)       
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required')
+    } else {
+        setSuccess(password)       
+    }
+};
